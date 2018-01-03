94458fc5d9934e0ba34d7c8f076bec3da01ad953
Carly Sierra <carlylwomack@gmail.com>
2018-01-02 20:31:02 -0600

    consolidating files

--- test/test-server.js
+++ test/test-server.js
@@ -11,7 +11,7 @@ chai.use(chaiHttp);
 describe('index page', function () {
   it('should exist', function () {
     return chai.request(app)
-      .get('/dashboard.html')
+      .get('/index.html')
       .then(function (res) {
         res.should.have.status(200);
       });
