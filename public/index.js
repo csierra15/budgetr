4636ac8b5fd197b0cb77d6193be69464864d37c0
Carly Sierra <carlylwomack@gmail.com>
2018-01-11 20:48:57 -0600

    changes

--- public/index.js
+++ public/index.js
@@ -12,7 +12,7 @@ function processTransactionData(transactionData) {
     transactions = Object.values(transactionData.transactions);
     return expenses;
 }
-// GET /expenses - return list of transactions for current month
+
 // GET /expenses/:date - return list of transactions for selected month
 
 function getTransactions() {
@@ -28,12 +28,14 @@ function displayTransactions() {
     console.log('displayTransactions ran');
     const newHtml = transactions.map(transaction => {
         let amount = parseFloat(transaction.amount).toFixed(2);
+        let date = moment(transaction.date).format('L')
         return `
-            <tr>` +
-                `<td class="description">${transaction.description}</td>` + 
-                `<td class="amount">$${amount}</td>` +
-                `<td class="purchaseDate">${transaction.date}</td>` +
-                `<td class="category">${transaction.category}</td>` +
+            <tr class="transaction" data-trans_id="${transaction.id}">` +
+                `<td class="description" contenteditable="true">${transaction.description}</td>` + 
+                `<td class="amount" contenteditable="true">$${amount}</td>` +
+                `<td class="purchaseDate" contenteditable="true">${date}</td>` +
+                `<td class="category" contenteditable="true">${transaction.category}</td>` +
+                `<td class="remove-tr"><button class="remove-trans-btn">x</button></td>` +
             `</tr>`
     });
 
@@ -66,7 +68,7 @@ function addTransaction() {
         category: category};
     console.log(newTransaction);
     $.ajax({
-        type: 'POST',
+        method: 'POST',
         url: '/transactions',
         contentType: 'application/JSON',
         data: JSON.stringify(newTransaction),
@@ -82,12 +84,31 @@ function addTransaction() {
     });
 }
 
-function updateTransaction() {
-
+function updateTransaction(trans_id) {
+    $.ajax({
+        method: 'PUT',
+        url: '/transactions/${trans_id}',
+        contentType: 'application/JSON',
+        data: '',
+        success: function() {
+            displayTransactions();
+        },
+        error: err => {
+            console.log(err);
+        }
+    });
 }
 
-function deleteTransaction() {
-    
+function deleteTransaction(trans_id) {
+    $.ajax({
+        method: 'DELETE',
+        url: '/transactions/${trans_id}',
+        data: '${trans_id}',
+        success: console.log('Transaction ${trans_id} successfully deleted'),
+        error: err => {
+            console.log('err')
+        }
+    })
 }
 
 //===== GOALS ======
@@ -117,7 +138,7 @@ function displayGoals() {
                     <button class="edit-goal-btn">Edit</button>
                     <button class="complete-goal">Completed</button>
                 </div>
-                <div class="goal-form">
+                <div class="edit-goal-form">
                     <input type="text" value="${goal.goal}" id="goal_${goal.id}"/>
                     <button type="submit" class="update-goal-btn">Save</button>
                 </div>
@@ -135,10 +156,10 @@ function getAndDisplayGoals() {
 }
 
 function addGoal() {
-    let goal= $('#goal-input').val();
-    
+    let goal = $('#goal-input').val();
+
     $.ajax({
-        type: 'POST',
+        method: 'POST',
         url: '/goals',
         contentType: 'application/JSON',
         data: JSON.stringify({goal: goal}),
@@ -148,17 +169,39 @@ function addGoal() {
             displayGoals();
         },
         error: (err) => {
+            console.log(err);
             $('#new-goal-section').append(`<p>Couldn't add goal :(</p>`);
         }
     });
 }
 
-function updateGoals() {
-    //update server with PUT endpoint
+function updateGoal(new_goal, goal_id) {
+    console.log('Updating goal');
+    $.ajax({
+        method: 'PUT',
+        url: '/goals/${goal_id}',
+        success: function() {
+            displayGoals();
+        },
+        error: err => {
+            console.log(err);
+            $('.goals').closest().append(`<p>Couldn't update goal :(</p>`);
+        }
+    })
 }
 
-function deleteGoal(){
-
+function deleteGoal(goal_id){
+    console.log('deleting goal ${goal_id}');
+    $.ajax({
+        method: 'DELETE',
+        url: '/goals/${goal_id}',
+        success: function() {
+            displayGoals();
+        },
+        error: err => {
+            console.log(err);
+        }
+    })
 }
 
 // ==== CALCULATE BUDGET (income - expenses) ====
@@ -167,19 +210,78 @@ function calculateBudget() {
     const totalExpenses = transactions.filter(transaction => transaction.category != 'income').map(transaction => transaction.amount).reduce((a, b) => {
         return parseFloat(a) + parseFloat(b);
     });
-
-    const totalIncome = transactions.filter(transaction => transaction.category === 'income').map(income => income.amount).reduce((a, b) => {
+    const totalIncome = transactions.filter(transaction => transaction.category === 'income').map(transaction => transaction.amount).reduce((a, b) => {
         return parseFloat(a) + parseFloat(b);
     });
-
-    const total = totalIncome - totalExpenses;
-
-    const totalBudget = total.toFixed(2);
+    let total = totalIncome - totalExpenses;
+    let totalBudget = total.toFixed(2);
 
     $(".budget-data").html("$" + totalBudget);
 }
 
+function handleNewTransaction() {
+    $('.trans-form').submit( e => {
+        if($('#select-category').val() === '0'){
+            $('#select-cat-message').show();
+            e.preventDefault();
+        }else{
+            e.preventDefault();
+            addTransaction();
+            $('#select-cat-message').hide();
+            $('#new-trans-section').hide();
+        }
+    });
+}
+
+function handleTransactionUpdate() {
+
+}
+
+function handleTransactionDelete() {
+    $('table').on('click', '.remove-trans-btn', e => {
+        e.preventDefault();
+        let trans_id = $(e.target).closest('.transaction').data('trans_id');
+        console.log('You clicked delete transaction');
+        $(e.target).closest('tr').remove();
+        deleteTransaction(trans_id);
+    });
+}
+
+function handleNewGoal() {
+    $('#submit-goal-btn').on('click', e => {
+        e.preventDefault();
+        addGoal();
+        $('#new-goal-section').hide();
+    })
+}
+
+function handleUpdateGoal() {
+    $('.goals').on('click', '.update-goal-btn', e => {
+        //1. save the goal locally
+        let goal_id = $(e.target).closest('.goals').data('goal_id');
+        let new_goal = $('#goal_' + goal_id).val();
+        console.log(goal_id, new_goal);
+        let edited_goal = goals.find(goal => goal.id == goal_id);
+        console.log(edited_goal);
+        edited_goal.goal = new_goal;
+        addGoal(new_goal, goal_id);
+        $('.editable').removeClass('editable');
+    });
+}
+ 
+function handleDeleteGoal() {
+    $('.goals').on('click', '.complete-goal', e => {
+        console.log('You completed a goal!');
+        e.preventDefault();
+        let goal_id = $(e.target).closest('.goals').data('goal_id');
+        deleteGoal(goal_id);
+        $(e.target).closest('.goals').detach();
+    });
+}
+
 $(function() {
+
+    let loggedIn;
 
     $('#login').hide();
     $('#register').hide();
@@ -202,18 +304,6 @@ $(function() {
         $('#new-trans-section').show();
     });
 
-    $('.trans-form').submit( e => {
-        if($('#select-category').val() === '0'){
-            $('#select-cat-message').show();
-            e.preventDefault();
-        }else{
-            e.preventDefault();
-            addTransaction();
-            $('#select-cat-message').hide();
-            $('#new-trans-section').hide();
-        }
-    });
-
     $('#cancel-trans-btn').on('click', e => {
         $('#new-trans-section').hide();
     });
@@ -223,50 +313,33 @@ $(function() {
         $('#new-goal-section').show();
     });
 
-    $('#submit-goal-btn').on('click', e => {
-        e.preventDefault();
-        addGoal();
-        $('#new-goal-section').hide();
-    })
-
     $('#cancel-goal-btn').on('click', e => {
         $('#new-goal-section').hide();
     });
 
-    displayMonth()
-    Promise.all([
-        getAndDisplayTransactions()
-    ]).then(() => {
-      calculateBudget();
-    });
-    getAndDisplayGoals();
-  
-    $('.goals').on('click', '.complete-goal', e => {
-        console.log('You completed a goal!');
-        e.preventDefault();
-        $(e.target).closest('.goals').remove();
-    });
+    if(loggedIn === true) {
+        displayMonth()
+        Promise.all([
+            getAndDisplayTransactions()
+        ]).then(() => {
+        calculateBudget();
+        });
+        getAndDisplayGoals();
+    }
 
     $('.goals').on('click', '.edit-goal-btn', e => {
-        console.log('You completed a goal!');
         e.preventDefault();
         $('.editable').removeClass('.editable');
-        $(e.target).closest(".goals").addClass('editable');
+        $(e.target).closest('.goals').addClass('editable');
     });
 
-    $('.goals').on('click', '.update-goal-btn', e => {
-        //1. save the goal locally
-        let goal_id = $(e.target).closest('.goals').data('goal_id');
-        let new_goal = $('#goal_' + goal_id).val();
-        //1.5 validate
-        console.log(goal_id, new_goal);
-        let edited_goal = goals.find(goal => goal.id == goal_id);
-        //1.6 make sure that you did find the goal
-        edited_goal.goal = new_goal;
-        displayGoals();
-        //2. send this change to the server
-        //3. remove the form
-        $('.editable').removeClass('editable');
+    $(function() {
+        handleNewTransaction();
+        handleTransactionUpdate();
+        handleTransactionDelete();
+        handleNewGoal();
+        handleUpdateGoal();
+        handleDeleteGoal();
     });
 });
 
@@ -282,28 +355,4 @@ let lineChart = new Chart(LINE_CHART, {
     data: data,
     options: options
 });
-
-let doughnutChart = new Chart(DOUGHNUT_CHART, {
-    type: 'doughnut',
-    data: {
-        labels: ['Food', 'Entertainment', 'Emergency', 'Bills', 'Personal Care'],
-        datasets: [{
-            label: 'Money Spent',
-            backgroundColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)'],
-            data: purchases
-        }]
-
-    },
-    //options: {
-      //  animation: {
-        //    animateScale: true
-        //}
-    //}
-});
-
 */
