1bcc54a5ec9250c3b34b93465bfb39b5eec99811
Carly Sierra <carlylwomack@gmail.com>
2018-05-10 17:58:26 -0500

    changed domain name

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 'use strict';
 
-exports.DATABASE_URL = 
+exports.DATABASE_URL =
     process.env.DATABASE_URL ||
     global.DATABASE_URL ||
     'mongodb://localhost/budgetr';
-exports.PORT = process.env.PORT || 8080;exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY;
